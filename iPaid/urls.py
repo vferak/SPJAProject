b1dd600611fd3736a673835466df04107cb70b3a
vferak <vferak@gmail.com>
2019-12-11 20:42:41 +0100

    Navbar added, other changes

--- iPaid/urls.py
+++ iPaid/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import app.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', app.views.homepage, name='homepage'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
